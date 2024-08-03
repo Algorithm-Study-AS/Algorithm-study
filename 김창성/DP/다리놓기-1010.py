83f300ec7816d64b01c08e3cc31f839e7277fd77
kcs19 <changsung19@gmail.com>
2024-08-03 23:29:02 +0900

    [baekjoon-1463] 1로만들기

--- 김창성/DP/다리놓기-1010.py
+++ 김창성/DP/다리놓기-1010.py
@@ -1,11 +1,20 @@
-array = [[0]*30 for _ in range(30)]
+def bridge():
+    #초기화
+    array = [[0]*30 for _ in range(30)]
 
-for i in range(30):
-    array[0][i]=1+i
+    #N이 1일때, M반환
+    for i in range(30): 
+        array[0][i]=1+i
 
-for i in range(1,30):
-    for j in range(i,30):
-        array[i][j]=array[i][j-1]+array[i-1][j-1]
+    #N>=2일때, 
+    for i in range(1,30):
+        for j in range(i,30): #N<=M일때만
+            array[i][j]=array[i][j-1]+array[i-1][j-1]
+
+    return array
+
+
+array=bridge()
 
 repeat = int(input())
 
