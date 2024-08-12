f08729092dad2e36164e4b3d648dc72c98bf53d3
kcs19 <changsung19@gmail.com>
2024-08-12 23:31:08 +0900

    [baekjoon-2156] 포도주 시식

--- 김창성/구현/홀수 홀릭 호석-20164.py
+++ 김창성/구현/홀수 홀릭 호석-20164.py
@@ -1,3 +1,5 @@
+#숫자를 분할하면서 나오는 홀수 개수 구하기
+
 odd_count_max=float('-inf')
 odd_count_min=float('inf')
 
@@ -15,20 +17,20 @@ def odd_holic(N:str,total_count:int):
     
     odd_count=count_odd(N)
 
-    if len(N) == 1:
+    if len(N) == 1: #길이가 1일 때
         odd_count_min = min(total_count + odd_count, odd_count_min)
         odd_count_max = max(total_count + odd_count, odd_count_max)
             
-    elif len(N) == 2:
+    elif len(N) == 2: #길이가 2일 때
         sum=int(N[0])+int(N[1])
 
         odd_holic(str(sum), total_count + odd_count)
 
-    else: 
+    else: #길이가 3이상일 때
         num=int(N)
-        for i in range (len(N)-1,1,-1): #count~2
-            for j in range(i-1,0,-1): #count-1~1
-                value = num//(10**i)
+        for i in range (len(N)-1,1,-1): #len~2
+            for j in range(i-1,0,-1): #len-1~1
+                value = num//(10**i) 
                 value2 = (num-value*(10**i))//(10**j)
                 value3 = num-value2*(10**j)-value*(10**i)
                 sum = value+value2+value3
