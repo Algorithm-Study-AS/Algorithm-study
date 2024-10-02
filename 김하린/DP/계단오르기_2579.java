75dc2ce1b873f62a979136fd9ec9c5aafb182f62
harin1212 <harin2950@naver.com>
2024-10-02 13:44:32 +0900

    [Beakjoon-2579] 계단 오르기

--- 김하린/DP/계단오르기_2579.java
+++ 김하린/DP/계단오르기_2579.java
@@ -19,7 +19,7 @@ public class 계단오르기_2579 {
 
         int[] dp = new int[n+1];
         dp[1] = stairs[1];
-        dp[2] = stairs[1]+ stairs[2];
+        if(n>=2) dp[2] = stairs[1]+ stairs[2];
         for (int i=3; i<=n; i++){
             dp[i] = Math.max(dp[i-2],dp[i-3]+stairs[i-1]) + stairs[i];
         }
